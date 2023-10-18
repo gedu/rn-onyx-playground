17e5662e8fa91e419823fa97083b840020bcf280
Adam Horodyski <ad.horodyski@gmail.com>
2023-10-18 14:58:36 +0200

    feat: add withOnyx alternative to the demo

--- src/pages/jotai-onyx/JotaiOnyxScreen.tsx
+++ src/pages/jotai-onyx/JotaiOnyxScreen.tsx
@@ -3,7 +3,7 @@ import {View, Text, Button, ScrollView} from 'react-native';
 import {useAtomValue} from 'jotai';
 import {ONYX_KEYS} from '../../lib/onyx-keys';
 import {atomWithOnyx, useAtomWithOnyxById} from './atomWithOnyx';
-import Onyx from 'react-native-onyx';
+import Onyx, {withOnyx} from 'react-native-onyx';
 
 const personalDetailsAtom = atomWithOnyx<Record<string, unknown>>(
   ONYX_KEYS.PERSONAL_DETAILS,
@@ -22,7 +22,7 @@ function EditComponent() {
         title="Change subscribed item"
         onPress={() =>
           Onyx.merge(ONYX_KEYS.PERSONAL_DETAILS, {
-            '0': {name: `${getRandomName()} (subscribed)`},
+            '0': {name: getRandomName()},
           })
         }
       />
@@ -51,12 +51,40 @@ function JotaiProvidedKey() {
   });
 
   return (
-    <View style={{flex: 1, padding: 16, backgroundColor: 'lightgreen'}}>
-      <Text>Render count: {renderCounter.current}</Text>
-      <Text>{JSON.stringify(personalDetails, null, 2)}</Text>
+    <View style={{flex: 1, padding: 4, backgroundColor: 'lightgreen'}}>
+      <Text style={{fontWeight: 'bold'}}>
+        Render count: {renderCounter.current}
+      </Text>
+      <Text>Subscribed keys: {Object.keys(personalDetails).length}</Text>
     </View>
   );
 }
+
+/**
+ * This component uses withOnyx from 'react-native-onyx' to display data from Onyx (everything under a collection at key).
+ */
+function OnyxProvidedKey({personalDetails}: any) {
+  const renderCounter = useRef(0);
+
+  useEffect(() => {
+    renderCounter.current += 1;
+  });
+
+  return (
+    <View style={{flex: 1, padding: 4, backgroundColor: 'lightgreen'}}>
+      <Text style={{fontWeight: 'bold'}}>
+        Render count: {renderCounter.current}
+      </Text>
+      <Text>Subscribed keys: {Object.keys(personalDetails).length}</Text>
+    </View>
+  );
+}
+
+const WithOnyxProvidedKey = withOnyx<any, unknown>({
+  personalDetails: {
+    key: ONYX_KEYS.PERSONAL_DETAILS,
+  },
+})(OnyxProvidedKey);
 
 /**
  * This component uses Jotai to display the data for a given key on a collection stored in Onyx.
@@ -71,22 +99,69 @@ function JotaiProvidedKeySelector({id}: {id: string}) {
   });
 
   return (
-    <View style={{flex: 1, padding: 16, backgroundColor: 'pink'}}>
-      <Text>Render count: {renderCounter.current}</Text>
-      <Text>Name: {user?.name ?? 'Unknown'}</Text>
+    <View style={{flex: 1, padding: 4, backgroundColor: 'pink'}}>
+      <Text style={{fontWeight: 'bold', marginBottom: 4}}>
+        Render count: {renderCounter.current}
+      </Text>
+      <Text>Subscribed keys: 1</Text>
+      <Text>
+        Key: {id}, Name: {user?.name ?? 'Unknown'}
+      </Text>
     </View>
   );
 }
+
+/**
+ * This component uses withOnyx HOC from 'react-native-onyx' to display the data for a given key on a collection stored in Onyx.
+ * It will re-render when the data for the given key changes - updates to other keys will also trigger a re-render.
+ */
+function OnyxProvidedKeySelector({id, personalDetails}: any) {
+  const user = personalDetails[id];
+
+  const renderCounter = useRef(0);
+
+  useEffect(() => {
+    renderCounter.current += 1;
+  });
+
+  return (
+    <View style={{flex: 1, padding: 4, backgroundColor: 'pink'}}>
+      <Text style={{fontWeight: 'bold', marginBottom: 4}}>
+        Render count: {renderCounter.current}
+      </Text>
+      <Text>Subscribed keys: {Object.keys(personalDetails).length}</Text>
+      <Text>
+        Key: {id}, Name: {user?.name ?? 'Unknown'}
+      </Text>
+    </View>
+  );
+}
+
+const WithOnyxProvidedKeySelector = withOnyx<any, unknown>({
+  personalDetails: {
+    key: ONYX_KEYS.PERSONAL_DETAILS,
+  },
+})(OnyxProvidedKeySelector);
 
 const id = '0';
 
 function JotaiOnyxScreen() {
   return (
-    <ScrollView style={{flex: 1}}>
+    <ScrollView>
       <EditComponent />
-      <JotaiProvidedKeySelector id={id} />
-      <JotaiProvidedKeySelector id={id} />
-      <JotaiProvidedKey />
+      <View style={{flex: 1, rowGap: 2, marginBottom: 16}}>
+        <Text style={{fontSize: 20, marginBottom: 8}}>Jotai</Text>
+        <JotaiProvidedKey />
+        <JotaiProvidedKeySelector id={id} />
+        <JotaiProvidedKeySelector id={id} />
+      </View>
+
+      <View style={{flex: 1, rowGap: 2}}>
+        <Text style={{fontSize: 20, marginBottom: 8}}>withOnyx</Text>
+        <WithOnyxProvidedKey />
+        <WithOnyxProvidedKeySelector id={id} />
+        <WithOnyxProvidedKeySelector id={id} />
+      </View>
     </ScrollView>
   );
 }
